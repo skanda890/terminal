eee9d678e61511745516d641e63b39206099ad4c
Leonard Hecker <lhecker@microsoft.com>
2024-03-20 16:15:22 +0100

    wip

--- src/types/CodepointWidthDetector_triegen.go
+++ src/types/CodepointWidthDetector_triegen.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"math"
@@ -15,37 +16,31 @@ import (
 type CharacterWidth int
 
 const (
-	CW_ZERO_WIDTH CharacterWidth = iota
-	CW_NARROW
-	CW_WIDE
-	CW_AMBIGUOUS
+	cwZeroWidth CharacterWidth = iota
+	cwNarrow
+	cwWide
+	cwAmbiguous
 )
 
 type ClusterBreak int
 
 const (
-	CB_OTHER ClusterBreak = iota
-	CB_CONTROL
-	CB_EXTEND
-	CB_PREPEND
-	CB_ZERO_WIDTH_JOINER
-	CB_REGIONAL_INDICATOR
-	CB_HANGUL_L
-	CB_HANGUL_V
-	CB_HANGUL_T
-	CB_HANGUL_LV
-	CB_HANGUL_LVT
-	CB_CONJUNCT_LINKER
-	CB_EXTENDED_PICTOGRAPHIC
+	cbOther ClusterBreak = iota
+	cbControl
+	cbExtend
+	cbPrepend
+	cbZeroWidthJoiner
+	cbRegionalIndicator
+	cbHangulL
+	cbHangulV
+	cbHangulT
+	cbHangulLV
+	cbHangulLVT
+	cbConjunctLinker
+	cbExtendedPictographic
 
-	CB_COUNT
+	cbCount
 )
-
-type TrieType uint32
-
-func trieValue(cb ClusterBreak, width CharacterWidth) TrieType {
-	return TrieType(byte(cb) | byte(width)<<6)
-}
 
 type HexInt int
 
@@ -142,19 +137,17 @@ You can download the latest ucd.nounihan.grouped.xml from:
 	_, _ = fmt.Fprintf(buf, "// on %v, from %s, %d bytes\n", time.Now().UTC().Format(time.RFC3339), ucd.Description, totalSize)
 	_, _ = fmt.Fprintf(buf, "// clang-format off\n")
 
-	for stageIndex, s := range stages {
-		octals := s.Bits / 4
+	for i, s := range stages {
 		width := 16
-		if stageIndex != 0 {
+		if i != 0 {
 			width = s.Mask + 1
 		}
-
-		_, _ = fmt.Fprintf(buf, "static constexpr uint%d_t s_stage%d[] = {", s.Bits, stageIndex+1)
-		for valueIndex, value := range s.Values {
-			if valueIndex%width == 0 {
+		_, _ = fmt.Fprintf(buf, "static constexpr uint%d_t s_stage%d[] = {", s.Bits, i+1)
+		for j, value := range s.Values {
+			if j%width == 0 {
 				buf.WriteString("\n   ")
 			}
-			_, _ = fmt.Fprintf(buf, " 0x%0*x,", octals, value)
+			_, _ = fmt.Fprintf(buf, " 0x%0*x,", s.Bits / 4, value)
 		}
 		buf.WriteString("\n};\n")
 	}
@@ -170,39 +163,44 @@ You can download the latest ucd.nounihan.grouped.xml from:
 			_, _ = fmt.Fprintf(buf, " %d,", i)
 		}
 	}
-	_, _ = fmt.Fprintf(buf, "\n};\n")
+	buf.WriteString("\n};\n")
 
-	_ = (`
-{{end}}
+	buf.WriteString("[[msvc::forceinline]] constexpr uint8_t ucdLookup(const char32_t cp) noexcept\n")
+	buf.WriteString("{\n")
+	for i, s := range stages {
+		_, _ = fmt.Fprintf(buf, "    const auto s%d = s_stage%d[", i+1, i+1)
+		if i == 0 {
+			_, _ = fmt.Fprintf(buf, "cp >> %d", s.Shift)
+		} else {
+			_, _ = fmt.Fprintf(buf, "s%d + ((cp >> %d) & %d)", i, s.Shift, s.Mask)
+		}
+		buf.WriteString("];\n")
+	}
+	buf.WriteString("}\n")
 
-[[msvc::forceinline]] constexpr uint8_t ucdLookup(const char32_t cp) noexcept
-{
-    const auto s1 = s_stage1[cp >> {stage1_shift}];
-    const auto s2 = s_stage2[s1 + ((cp >> {stage2_shift}) & {stage2_mask})];
-    const auto s3 = s_stage3[s2 + ((cp >> {stage3_shift}) & {stage3_mask})];
-    const auto s4 = s_stage4[s3 + (cp & {stage4_mask})];
-    return s4;
-}
-[[msvc::forceinline]] constexpr uint8_t ucdGraphemeJoins(const uint8_t lead, const uint8_t trail) noexcept
-{
-    const auto l = lead & 15;
-    const auto t = trail & 15;
-    return s_joinRules[l][t];
-}
-[[msvc::forceinline]] constexpr int ucdToCharacterWidth(const uint8_t val) noexcept
-{
-    return (val >> {CHARACTER_WIDTH_SHIFT}) & {CHARACTER_WIDTH_MASK};
-}
-// clang-format on`)
+	buf.WriteString("[[msvc::forceinline]] constexpr uint8_t ucdGraphemeJoins(const uint8_t lead, const uint8_t trail) noexcept\n")
+	buf.WriteString("{\n")
+	buf.WriteString("    const auto l = lead & 15;\n")
+	buf.WriteString("    const auto t = trail & 15;\n")
+	buf.WriteString("    return s_joinRules[l][t];\n")
+	buf.WriteString("}\n")
 
-	_ = rules
-	fmt.Println(buf.String())
+	buf.WriteString("[[msvc::forceinline]] constexpr int ucdToCharacterWidth(const uint8_t val) noexcept\n")
+	buf.WriteString("{\n")
+	buf.WriteString("    return val >> 6;\n")
+	buf.WriteString("}\n")
+
+	buf.WriteString("// clang-format on\n")
+
+	_, _ = os.Stdout.WriteString(buf.String())
 	return nil
 }
 
+type TrieType uint32
+
 func extractValuesFromUCD(ucd *UCD) ([]TrieType, error) {
 	values := make([]TrieType, 1114112)
-	fillRange(values, trieValue(CB_OTHER, CW_NARROW))
+	fillRange(values, trieValue(cbOther, cwNarrow))
 
 	for _, group := range ucd.Repertoire.Group {
 		for _, char := range group.Char {
@@ -217,8 +215,6 @@ func extractValuesFromUCD(ucd *UCD) ([]TrieType, error) {
 				firstCp, lastCp = int(char.Codepoint), int(char.Codepoint)
 			}
 
-			// <reserved cp="2065" age="unassigned" na="" gc="Cn" bc="BN" lb="XX" sc="Zzzz" scx="Zzzz" DI="Y" ODI="Y" Gr_Base="N" Pat_Syn="N" GCB="CN" CWKCF="Y" NFKC_CF="" vo="U" NFKC_SCF=""/>
-
 			var (
 				cb    ClusterBreak
 				width CharacterWidth
@@ -226,30 +222,30 @@ func extractValuesFromUCD(ucd *UCD) ([]TrieType, error) {
 
 			switch graphemeClusterBreak {
 			case "XX": // Anything else
-				cb = CB_OTHER
+				cb = cbOther
 			case "CR", "LF", "CN": // Carriage Return, Line Feed, Control
 				// We ignore GB3 which demands that CR × LF do not break apart, because
 				// a) these control characters won't normally reach our text storage
 				// b) otherwise we're in a raw write mode and historically conhost stores them in separate cells
-				cb = CB_CONTROL
+				cb = cbControl
 			case "EX", "SM": // Extend, SpacingMark
-				cb = CB_EXTEND
+				cb = cbExtend
 			case "PP": // Prepend
-				cb = CB_PREPEND
+				cb = cbPrepend
 			case "ZWJ": // Zero Width Joiner
-				cb = CB_ZERO_WIDTH_JOINER
+				cb = cbZeroWidthJoiner
 			case "RI": // Regional Indicator
-				cb = CB_REGIONAL_INDICATOR
+				cb = cbRegionalIndicator
 			case "L": // Hangul Syllable Type L
-				cb = CB_HANGUL_L
+				cb = cbHangulL
 			case "V": // Hangul Syllable Type V
-				cb = CB_HANGUL_V
+				cb = cbHangulV
 			case "T": // Hangul Syllable Type T
-				cb = CB_HANGUL_T
+				cb = cbHangulT
 			case "LV": // Hangul Syllable Type LV
-				cb = CB_HANGUL_LV
+				cb = cbHangulLV
 			case "LVT": // Hangul Syllable Type LVT
-				cb = CB_HANGUL_LVT
+				cb = cbHangulLVT
 			default:
 				return nil, fmt.Errorf("unrecognized GCB %s for %U to %U", graphemeClusterBreak, firstCp, lastCp)
 			}
@@ -258,27 +254,27 @@ func extractValuesFromUCD(ucd *UCD) ([]TrieType, error) {
 				// Currently every single Extended_Pictographic codepoint happens to be GCB=XX.
 				// This is fantastic for us because it means we can stuff it into the ClusterBreak enum
 				// and treat it as an alias of EXTEND, but with the special GB11 properties.
-				if cb != CB_OTHER {
+				if cb != cbOther {
 					return nil, fmt.Errorf("unexpected GCB %s for ExtPict for %U to %U", graphemeClusterBreak, firstCp, lastCp)
 				}
-				cb = CB_EXTENDED_PICTOGRAPHIC
+				cb = cbExtendedPictographic
 			}
 
 			if indicConjunctBreak == "Linker" {
 				// Similarly here, we can treat it as an alias for EXTEND, but with the GB9c properties.
-				if cb != CB_EXTEND {
+				if cb != cbExtend {
 					return nil, fmt.Errorf("unexpected GCB %s for InCB=Linker for %U to %U", graphemeClusterBreak, firstCp, lastCp)
 				}
-				cb = CB_CONJUNCT_LINKER
+				cb = cbConjunctLinker
 			}
 
 			switch eastAsian {
 			case "N", "Na", "H": // neutral, narrow, half-width
-				width = CW_NARROW
+				width = cwNarrow
 			case "F", "W": // full-width, wide
-				width = CW_WIDE
+				width = cwWide
 			case "A": // ambiguous
-				width = CW_AMBIGUOUS
+				width = cwAmbiguous
 			default:
 				return nil, fmt.Errorf("unrecognized ea %s for %U to %U", eastAsian, firstCp, lastCp)
 			}
@@ -289,7 +285,7 @@ func extractValuesFromUCD(ucd *UCD) ([]TrieType, error) {
 			//   Mn: Mark, non-spacing
 			//   Cf: Control, format
 			if strings.HasPrefix(generalCategory, "M") || generalCategory == "Cf" {
-				width = CW_ZERO_WIDTH
+				width = cwZeroWidth
 			}
 
 			fillRange(values[firstCp:lastCp+1], trieValue(cb, width))
@@ -298,13 +294,24 @@ func extractValuesFromUCD(ucd *UCD) ([]TrieType, error) {
 
 	// Box-drawing and block elements require 1-cell alignment.
 	// Most characters in this range have an ambiguous width otherwise.
-	fillRange(values[0x2500:0x259F+1], trieValue(CB_OTHER, CW_NARROW))
+	fillRange(values[0x2500:0x259F+1], trieValue(cbOther, cwNarrow))
 	// hexagrams are historically narrow
-	fillRange(values[0x4DC0:0x4DFF+1], trieValue(CB_OTHER, CW_NARROW))
+	fillRange(values[0x4DC0:0x4DFF+1], trieValue(cbOther, cwNarrow))
 	// narrow combining ligatures (split into left/right halves, which take 2 columns together)
-	fillRange(values[0xFE20:0xFE2F+1], trieValue(CB_OTHER, CW_NARROW))
+	fillRange(values[0xFE20:0xFE2F+1], trieValue(cbOther, cwNarrow))
 
 	return values, nil
+}
+
+func trieValue(cb ClusterBreak, width CharacterWidth) TrieType {
+	return TrieType(byte(cb) | byte(width)<<6)
+}
+
+func coalesce(a, b string) string {
+	if a != "" {
+		return a
+	}
+	return b
 }
 
 type Stage struct {
@@ -312,7 +319,6 @@ type Stage struct {
 	Shift  int
 	Mask   int
 	Bits   int
-	Index  int
 }
 
 func buildTrie(uncompressed []TrieType, shifts []int) []*Stage {
@@ -322,19 +328,28 @@ func buildTrie(uncompressed []TrieType, shifts []int) []*Stage {
 	for _, shift := range shifts {
 		chunkSize := 1 << shift
 		cache := map[string]TrieType{}
-		offsets := []TrieType(nil)
-		compressed := []TrieType(nil)
+		compressed := make([]TrieType, 0, len(uncompressed)/8)
+		offsets := make([]TrieType, 0, len(uncompressed)/chunkSize)
 
 		for i := 0; i < len(uncompressed); i += chunkSize {
-			chunk := uncompressed[i:min(i+chunkSize, len(uncompressed))]
-			// Cast the integer slice to a string so that it's hashable.
+			chunk := uncompressed[i : i+chunkSize]
+			// Cast the integer slice to a string so that it can be hashed.
 			key := unsafe.String((*byte)(unsafe.Pointer(&chunk[0])), len(chunk)*int(unsafe.Sizeof(chunk[0])))
-
 			offset, exists := cache[key]
+
 			if !exists {
-				offset = TrieType(len(compressed))
-				cache[key] = offset
-				compressed = append(compressed, chunk...)
+				// For a 4-stage trie searching for existing occurrences of chunk in compressed yields a ~10%
+				// compression improvement. Checking for overlaps with the tail end of compressed yields another ~15%.
+				// FYI I tried to shuffle the order of compressed chunks but found that this has a negligible impact.
+				if existing := findExisting(compressed, chunk); existing != -1 {
+					offset = TrieType(existing)
+					cache[key] = offset
+				} else {
+					overlap := measureOverlap(compressed, chunk)
+					compressed = append(compressed, chunk[overlap:]...)
+					offset = TrieType(len(compressed) - len(chunk))
+					cache[key] = offset
+				}
 			}
 
 			offsets = append(offsets, offset)
@@ -353,20 +368,58 @@ func buildTrie(uncompressed []TrieType, shifts []int) []*Stage {
 	stages = append(stages, &Stage{
 		Values: uncompressed,
 		Shift:  cumulativeShift,
-		Mask:   math.MaxUint32,
+		Mask:   math.MaxInt32,
 	})
 
-	slices.Reverse(stages)
-
-	for i, s := range stages {
-		s.Bits = bitSize(slices.Max(s.Values))
-		s.Index = i + 1
+	for _, s := range stages {
+		m := slices.Max(s.Values)
+		if m <= 0xff {
+			s.Bits = 8
+		} else if m <= 0xffff {
+			s.Bits = 16
+		} else {
+			s.Bits = 32
+		}
 	}
 
+	slices.Reverse(stages)
 	return stages
 }
 
-func buildJoinRules() [CB_COUNT][CB_COUNT]bool {
+// Finds needle in haystack. Returns -1 if it couldn't be found.
+func findExisting(haystack, needle []TrieType) int {
+	if len(haystack) == 0 || len(needle) == 0 {
+		return -1
+	}
+
+	s := int(unsafe.Sizeof(TrieType(0)))
+	h := unsafe.Slice((*byte)(unsafe.Pointer(&haystack[0])), len(haystack)*s)
+	n := unsafe.Slice((*byte)(unsafe.Pointer(&needle[0])), len(needle)*s)
+	i := 0
+
+	for {
+		i = bytes.Index(h[i:], n)
+		if i == -1 {
+			return -1
+		}
+		if i%s == 0 {
+			return i / s
+		}
+	}
+}
+
+// Given two slices, this returns the amount by which prev's end overlaps with next's start.
+// That is, given [0,1,2,3,4] and [2,3,4,5] this returns 3 because [2,3,4] is the "overlap".
+func measureOverlap(prev, next []TrieType) int {
+	for overlap := min(len(prev), len(next)); overlap >= 0; overlap-- {
+		if slices.Equal(prev[len(prev)-overlap:], next[:overlap]) {
+			return overlap
+		}
+	}
+	return 0
+}
+
+func buildJoinRules() [cbCount][cbCount]bool {
 	// UAX #29 states:
 	// > Note: Testing two adjacent characters is insufficient for determining a boundary.
 	//
@@ -383,7 +436,7 @@ func buildJoinRules() [CB_COUNT][CB_COUNT]bool {
 
 	// Otherwise, break everywhere.
 	// GB999: Any ÷ Any
-	var rules [CB_COUNT][CB_COUNT]bool
+	var rules [cbCount][cbCount]bool
 
 	// Do not break within emoji flag sequences. That is, do not break between regional indicator
 	// (RI) symbols if there is an odd number of RI characters before the break point.
@@ -391,13 +444,13 @@ func buildJoinRules() [CB_COUNT][CB_COUNT]bool {
 	// GB12: sot (RI RI)* RI × RI
 	//
 	// We cheat here by not checking that the number of RIs is even. Meh!
-	rules[CB_REGIONAL_INDICATOR][CB_REGIONAL_INDICATOR] = true
+	rules[cbRegionalIndicator][cbRegionalIndicator] = true
 
 	// Do not break within emoji modifier sequences or emoji zwj sequences.
 	// GB11: \p{Extended_Pictographic} Extend* ZWJ × \p{Extended_Pictographic}
 	//
 	// We cheat here by not checking that the ZWJ is preceded by an ExtPic. Meh!
-	rules[CB_ZERO_WIDTH_JOINER][CB_EXTENDED_PICTOGRAPHIC] = true
+	rules[cbZeroWidthJoiner][cbExtendedPictographic] = true
 
 	// Do not break within certain combinations with Indic_Conjunct_Break (InCB)=Linker.
 	// GB9c: \p{InCB=Consonant} [\p{InCB=Extend}\p{InCB=Linker}]* \p{InCB=Linker} [\p{InCB=Extend}\p{InCB=Linker}]* × \p{InCB=Consonant}
@@ -405,48 +458,48 @@ func buildJoinRules() [CB_COUNT][CB_COUNT]bool {
 	// I'm sure GB9c is great for these languages, but honestly the definition is complete whack.
 	// Just look at that chonker! This isn't a "cheat" like the others above, this is a reinvention:
 	// We treat it as having both ClusterBreak.PREPEND and ClusterBreak.EXTEND properties.
-	fillRange(rules[CB_CONJUNCT_LINKER][:], true)
-	for _, lead := range rules {
-		lead[CB_CONJUNCT_LINKER] = true
+	fillRange(rules[cbConjunctLinker][:], true)
+	for i := range rules {
+		rules[i][cbConjunctLinker] = true
 	}
 
 	// Do not break before SpacingMarks, or after Prepend characters.
 	// GB9b: Prepend ×
-	fillRange(rules[CB_PREPEND][:], true)
+	fillRange(rules[cbPrepend][:], true)
 
 	// Do not break before SpacingMarks, or after Prepend characters.
 	// GB9a: × SpacingMark
 	// Do not break before extending characters or ZWJ.
 	// GB9: × (Extend | ZWJ)
-	for _, lead := range rules {
+	for i := range rules {
 		// CodepointWidthDetector_triegen.py sets SpacingMarks to ClusterBreak.EXTEND as well,
 		// since they're entirely identical to GB9's Extend.
-		lead[CB_EXTEND] = true
-		lead[CB_ZERO_WIDTH_JOINER] = true
+		rules[i][cbExtend] = true
+		rules[i][cbZeroWidthJoiner] = true
 	}
 
 	// Do not break Hangul syllable sequences.
 	// GB8: (LVT | T) x T
-	rules[CB_HANGUL_LVT][CB_HANGUL_T] = true
-	rules[CB_HANGUL_T][CB_HANGUL_T] = true
+	rules[cbHangulLVT][cbHangulT] = true
+	rules[cbHangulT][cbHangulT] = true
 	// GB7: (LV | V) x (V | T)
-	rules[CB_HANGUL_LV][CB_HANGUL_T] = true
-	rules[CB_HANGUL_LV][CB_HANGUL_V] = true
-	rules[CB_HANGUL_V][CB_HANGUL_V] = true
-	rules[CB_HANGUL_V][CB_HANGUL_T] = true
+	rules[cbHangulLV][cbHangulT] = true
+	rules[cbHangulLV][cbHangulV] = true
+	rules[cbHangulV][cbHangulV] = true
+	rules[cbHangulV][cbHangulT] = true
 	// GB6: L x (L | V | LV | LVT)
-	rules[CB_HANGUL_L][CB_HANGUL_L] = true
-	rules[CB_HANGUL_L][CB_HANGUL_V] = true
-	rules[CB_HANGUL_L][CB_HANGUL_LV] = true
-	rules[CB_HANGUL_L][CB_HANGUL_LVT] = true
+	rules[cbHangulL][cbHangulL] = true
+	rules[cbHangulL][cbHangulV] = true
+	rules[cbHangulL][cbHangulLV] = true
+	rules[cbHangulL][cbHangulLVT] = true
 
 	// Do not break between a CR and LF. Otherwise, break before and after controls.
 	// GB5: ÷ (Control | CR | LF)
-	for _, lead := range rules {
-		lead[CB_CONTROL] = false
+	for i := range rules {
+		rules[i][cbControl] = false
 	}
 	// GB4: (Control | CR | LF) ÷
-	fillRange(rules[CB_CONTROL][:], false)
+	fillRange(rules[cbControl][:], false)
 
 	// We ignore GB3 which demands that CR × LF do not break apart, because
 	// a) these control characters won't normally reach our text storage
@@ -457,25 +510,8 @@ func buildJoinRules() [CB_COUNT][CB_COUNT]bool {
 	return rules
 }
 
-func coalesce(a, b string) string {
-	if a != "" {
-		return a
-	}
-	return b
-}
-
 func fillRange[T any](s []T, v T) {
 	for i := range s {
 		s[i] = v
 	}
-}
-
-func bitSize(x TrieType) int {
-	if x <= 0xff {
-		return 8
-	}
-	if x <= 0xffff {
-		return 16
-	}
-	return 32
 }
